7f4e77268d09af99d54116387bdb5c2507852fb6
Sergius <sergius@posteo.de>
2025-12-23 08:59:20 +0100

    desk Informationen zur keymaps.lua konfiguration hinzugefügt. Ist wichtig für das <leader>+ß binding

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,11 +4,11 @@
 local map = vim.keymap.set
 
 -- q für quit/close Aktionen nutzen
-map('n', 'q', '<Nop>')  -- Erst mal deaktivieren
-map('n', 'qq', ':q<CR>', { desc = "Quit" })
-map('n', 'qa', ':qa<CR>', { desc = "Quit all" })
-map('n', 'qw', ':wq<CR>', { desc = "Write and quit" })
-map('n', 'qQ', ':q!<CR>', { desc = "Force quit" })
+map('n', 'q', '<Nop>', { desc = "Deaktiviert (q für Quit-Aktionen)" })
+map('n', 'qq', ':q<CR>', { desc = "Beenden" })
+map('n', 'qa', ':qa<CR>', { desc = "Alle beenden" })
+map('n', 'qw', ':wq<CR>', { desc = "Speichern und beenden" })
+map('n', 'qQ', ':q!<CR>', { desc = "Erzwungen beenden" })
 
 -- Macros auf Leader-Kombinationen verschieben  m für "macro"
 -- map('n', '<leader>mr', 'q', { desc = "Macro record" })
@@ -42,27 +42,31 @@ map("n", "<leader>bp", ":bprevious<CR>", { desc = "Vorheriger Puffer" })
 -- Keybinding für nvim-tree (definiert in plugins/nerd-tree.lua)
 
 -- Datumstempel
-map('n', '<leader>dt', ':put=strftime(\'## %Y-%m-%d\')<CR>', { noremap = true })
+map('n', '<leader>dt', ':put=strftime(\'## %Y-%m-%d\')<CR>', { noremap = true, desc = "Datumstempel einfügen" })
 
 -- Anpassungen für's Deutsche Tastaturlayout
-map('n', 'YY', 'ZZ', { noremap = true })             -- Beenden mit Speichern
-map('n', 'YQ', 'ZQ', { noremap = true })             -- Beenden ohne Speichern
-map('n', '-', '/',   { noremap = true })             -- Suche (macht "-" zur Suchtaste statt "/")
-map('n', 'äs', '[s', { noremap = true })             -- Zum vorherigen Rechtschreibfehler springen
-map('n', 'ös', ']s', { noremap = true })             -- Zum nächsten Rechtschreibfehler springen
-map('n', 'z0', 'z=', { noremap = true })             -- Zeigt Wortvorschläge für Rechtschreibfehler
+map('n', 'YY', 'ZZ', { noremap = true, desc = "Beenden mit Speichern" })
+map('n', 'YQ', 'ZQ', { noremap = true, desc = "Beenden ohne Speichern" })
+map('n', '-', '/',   { noremap = true, desc = "Suche" })
+map('n', 'äs', '[s', { noremap = true, desc = "Vorheriger Rechtschreibfehler" })
+map('n', 'ös', ']s', { noremap = true, desc = "Nächster Rechtschreibfehler" })
+map('n', 'z0', 'z=', { noremap = true, desc = "Rechtschreibvorschläge" })
+
+-- Diff-Navigation (Deutsche Tastatur-Anpassung)
+map('n', 'öc', ']c', { noremap = true, desc = "Nächster Diff-Unterschied" })
+map('n', 'äc', '[c', { noremap = true, desc = "Vorheriger Diff-Unterschied" })
 
 -- === Funktionen ===
 -- vim.keymap.set('n', '<leader>t', ':term<CR>', { noremap = true })  -- Terminal öffnen (auskommentiert wegen zsh Problemen)
-map('', '<leader>s', ':setlocal spell! spelllang=de_de,en_us<CR>', { noremap = true })     -- Rechtschreibprüfung ein/ausschalten
-map('', '<leader>o', ':!clear && shellcheck -x %<CR>', { noremap = true })              -- Shellskripte mit shellcheck analysieren
+map('', '<leader>s', ':setlocal spell! spelllang=de_de,en_us<CR>', { noremap = true, desc = "Rechtschreibprüfung umschalten" })
+map('', '<leader>o', ':!clear && shellcheck -x %<CR>', { noremap = true, desc = "Shellcheck ausführen" })
 -- map('n', '<leader>h', ':call ToggleHiddenAll()<CR>', { noremap = true })                -- Versteckte Elemente ein-/ausblenden (benötigt die ToggleHiddenAll Funktion)
 
 -- Suchhervorhebung ausschalten mit Escape
-map('n', '<Esc>', ':nohlsearch<CR>', { noremap = true, silent = true })
+map('n', '<Esc>', ':nohlsearch<CR>', { noremap = true, silent = true, desc = "Suchhervorhebung ausschalten" })
 
 -- Neuen Tab öffnen
-map('n', '<leader>tn', ':tabnew<CR>', { noremap = true })
+map('n', '<leader>tn', ':tabnew<CR>', { noremap = true, desc = "Neuer Tab" })
 
 -- === Tastenkombinationen ===
 -- Platzhalter-Navigation (bewegt zum nächsten <++> und löscht es)
@@ -74,7 +78,7 @@ map('n', '<leader>tn', ':tabnew<CR>', { noremap = true })
 -- vim.keymap.set('', '<leader>r', ':vsp<space>$REFER<CR>', { noremap = true })     -- Öffnet Referenzdatei in vertikalem Split ($REFER muss definiert sein)
 
 -- Dokument-Workflow
-map('', '<leader>c', ':w! \\| !compiler "%:p"<CR>', { noremap = true })             -- Speichert und führt "compiler" mit aktuellem Dateipfad aus
+map('', '<leader>c', ':w! \\| !compiler "%:p"<CR>', { noremap = true, desc = "Speichern und kompilieren" })
 -- vim.keymap.set('', '<leader>p', ':!opout "%:p"<CR>', { noremap = true })         -- Führt "opout" mit aktuellem Dateipfad aus (vermutlich zum Anzeigen des Outputs)
 
 -- Terminal (funktionierendes Binding) Plugin testen
@@ -94,23 +98,23 @@ map('n', '<leader>gs', ':botright 12split | term git status<CR>', { desc = "Git 
 map('n', '<leader>gT', ':!cd "$(git rev-parse --show-toplevel 2>/dev/null || echo %:p:h)" && st -e sh -c "git status; $SHELL" &<CR><CR>', { desc = "ST mit Git Status" })
 
 -- Direkte Git-Befehle (ohne Terminal)
-map('n', '<leader>ga', ':!git add %<CR>', { desc = "Git add current file" })
-map('n', '<leader>gA', ':!git add -A<CR>', { desc = "Git add all" })
-map('n', '<leader>gc', ':!git commit<CR>', { desc = "Git commit (öffnet Editor)" })
+map('n', '<leader>ga', ':!git add %<CR>', { desc = "Git add aktuelle Datei" })
+map('n', '<leader>gA', ':!git add -A<CR>', { desc = "Git add alle Dateien" })
+map('n', '<leader>gc', ':!git commit<CR>', { desc = "Git commit" })
 map('n', '<leader>gp', ':!git push<CR>', { desc = "Git push" })
 map('n', '<leader>gl', ':!git pull<CR>', { desc = "Git pull" })
 
 -- Git Diff Varianten
-map('n', '<leader>gd', ':!git diff %<CR>', { desc = "Git diff current file" })
-map('n', '<leader>gD', ':!git diff<CR>', { desc = "Git diff all" })
+map('n', '<leader>gd', ':!git diff %<CR>', { desc = "Git diff aktuelle Datei" })
+map('n', '<leader>gD', ':!git diff<CR>', { desc = "Git diff alle Dateien" })
 
 -- Git Log schön formatiert
 map('n', '<leader>gL', ':botright 20split | term git log --oneline --graph --decorate -20<CR>', { desc = "Git log (letzte 20)" })
 
 -- Praktisch: Git-Befehle mit Fugitive-ähnlicher Syntax (wenn du kein Plugin willst)
-map('n', '<leader>gb', ':!git blame %<CR>', { desc = "Git blame current file" })
-map('n', '<leader>gh', ':!git log -p -1 %<CR>', { desc = "Git history current file" })
-map('n', '<leader>t', '<Cmd>ToggleTerm<CR>', { desc = "Toggle Terminal" })
+map('n', '<leader>gb', ':!git blame %<CR>', { desc = "Git blame aktuelle Datei" })
+map('n', '<leader>gh', ':!git log -p -1 %<CR>', { desc = "Git history aktuelle Datei" })
+map('n', '<leader>t', '<Cmd>ToggleTerm<CR>', { desc = "Terminal umschalten" })
 
 -- Which-key manuell öffnen
 map('n', '<leader>ß', '<Cmd>WhichKey<CR>', { desc = "Zeige alle Keybindings" })
